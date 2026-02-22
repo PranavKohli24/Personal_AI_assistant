c582d43e5e7c2e8f4ed5d56b224f2dd1e6f0695a
Pranav Kohli <kohlipranav24@gmail.com>
2026-02-22 09:01:18 +0000

    errror fixed

--- app/api/chat/get/route.js
+++ app/api/chat/get/route.js
@@ -1,14 +1,14 @@
 import connectDB from "@/config/db";
 import Chat from "@/models/Chat";
 
-import {getAuth} from "@clerk/nextjs/server";
+import {auth} from "@clerk/nextjs/server";
 
 import { NextResponse } from "next/server";
 
 export async function GET(req){
 
     try{
-        const {userId}=getAuth(req);
+        const {userId}=auth();
 
         if (!userId){
             return NextResponse.json({success:false, message:'User not Authenticated'})
@@ -16,7 +16,8 @@ export async function GET(req){
 
         //connect to the database and fetch all chats for the user.
         await connectDB();
-        const data=await Chat.find({userId});
+        // const data=await Chat.find({userId});
+        const data = await Chat.find({ userId }).sort({ updatedAt: -1 });
 
         return NextResponse.json({success:true, data})
     }catch(err){
